b5e818e5bf67293f739571814f4f40032d82af39
asim-sanzz <153579208+asim-sanzz@users.noreply.github.com>
2025-06-09 10:00:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama",
+      "name": "Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.15928161133493113,
+        "pitch": 0.012156233374678749,
+        "fov": 0.7341989679420979
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.408666119152727,
+          "pitch": 0.04124760062949662,
+          "rotation": 0,
+          "target": "1-panorama1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama1",
+      "name": "Panorama(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.511205836404633,
+        "pitch": 0.0034536878839475094,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
